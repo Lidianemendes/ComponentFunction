4cd03b551e6d9a93f00d6238a16379203c1a2d20
Lidianemendes <93939970+Lidianemendes@users.noreply.github.com>
2022-02-02 23:56:47 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function WelcomeClass() {
+  return (
+    <main>
+      <h1>A melhor turma.</h1>
+      <p>Segue abaixo as qualidades da T3:</p>
+      <ul>
+        <li> trabalho em equipe</li>
+        <li> animação</li>
+        <li> ninquém fica pra trás</li>
+      </ul>
+    </main>
+  );
+}
+
+export default WelcomeClass;
